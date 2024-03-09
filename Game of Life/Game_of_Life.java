352892e9bbe317a296f5a7dee39bbdeae68e77ee
Rukkhadevata123 <3230102179@zju.edu.cn>
2024-03-09 22:40:26 +0800

    newgame

---
+++ Game of Life/Game_of_Life.java
@@ -0,0 +1,80 @@
+public class Game_of_Life {
+
+    int[][] board;
+
+    public Game_of_Life(int row, int col) {
+        board = new int[row][col];
+    } 
+
+    public void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = (int) (Math.random() * 2);
+            }
+        }
+    }
+
+    public void updateBoard() {
+        int[][] newBoard = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                int liveNeighbors = countLiveNeighbors(i, j);
+                if (board[i][j] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3) {
+                        newBoard[i][j] = 0;
+                    } else {
+                        newBoard[i][j] = 1;
+                    }
+                } else {
+                    if (liveNeighbors == 3) {
+                        newBoard[i][j] = 1;
+                    } else {
+                        newBoard[i][j] = 0;
+                    }
+                }
+            }
+        }
+        board = newBoard;
+    }
+
+    public int countLiveNeighbors(int row, int col) {
+        int count = 0;
+        for (int i = row - 1; i <= row + 1; i++) {
+            for (int j = col - 1; j <= col + 1; j++) {
+                if (i >= 0 && i < board.length && j >= 0 && j < board[0].length) {
+                    if (i != row || j != col) {
+                        count += board[i][j];
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public void startGame(int numIterations) {
+        for (int i = 0; i < numIterations; i++) {
+            printBoard();
+            System.out.println();
+            updateBoard();
+        }
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public static void main(String[] args) {
+        Game_of_Life game = new Game_of_Life(5, 5);
+        game.initializeBoard();
+        game.startGame(5);
+    }
+}
---
+++ Game of Life/Game_of_Life_GUI.java
@@ -0,0 +1,115 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Game_of_Life_GUI extends JFrame {
+
+    private Game_of_Life game;
+    private JTextField rowField, colField, iterField;
+    private JTextField speedField;
+    private GamePanel gamePanel;
+    private JFrame gameFrame;
+    private Timer timer;
+
+    public Game_of_Life_GUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel inputPanel = new JPanel();
+        rowField = new JTextField(5);
+        colField = new JTextField(5);
+        iterField = new JTextField(5);
+        speedField = new JTextField(5);
+        JButton startButton = new JButton("开始游戏");
+
+        inputPanel.add(new JLabel("行数:"));
+        inputPanel.add(rowField);
+        inputPanel.add(new JLabel("列数:"));
+        inputPanel.add(colField);
+        inputPanel.add(new JLabel("重复次数:"));
+        inputPanel.add(iterField);
+        inputPanel.add(new JLabel("速度(ms):")); 
+        inputPanel.add(speedField); 
+        inputPanel.add(startButton);
+
+        add(inputPanel, BorderLayout.NORTH);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int rows = Integer.parseInt(rowField.getText());
+                int cols = Integer.parseInt(colField.getText());
+                int iterations = Integer.parseInt(iterField.getText());
+                int speed = Integer.parseInt(speedField.getText());
+
+                game = new Game_of_Life(rows, cols);
+                game.initializeBoard();
+
+                gamePanel = new GamePanel();
+                gamePanel.setBoard(game.getBoard());
+
+                gameFrame = new JFrame();
+                gameFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                gameFrame.add(gamePanel);
+                gameFrame.setTitle("生命游戏 - 游戏窗口");
+                gameFrame.setSize(cols * 40, rows * 40);
+                gameFrame.setLocationRelativeTo(null);
+                gameFrame.setVisible(true);
+
+                timer = new Timer(speed, new ActionListener() {
+                    int iter = 0;
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (iter < iterations) {
+                            game.updateBoard();
+                            gamePanel.setBoard(game.getBoard());
+                            gamePanel.repaint();
+                            iter++;
+                        } else {
+                            ((Timer) e.getSource()).stop();
+                        }
+                    }
+                });
+                timer.start();
+            }
+        });
+
+        setTitle("生命游戏 - 主窗口");
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Game_of_Life_GUI();
+    }
+
+    class GamePanel extends JPanel {
+        private int[][] board;
+
+        public void setBoard(int[][] board) {
+            this.board = board;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (board != null) {
+                int cellWidth = getWidth() / board[0].length;
+                int cellHeight = getHeight() / board.length;
+                for (int i = 0; i < board.length; i++) {
+                    for (int j = 0; j < board[0].length; j++) {
+                        if (board[i][j] == 1) {
+                            g.setColor(Color.RED);
+                            g.fillOval(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+                        }
+                        g.setColor(Color.BLACK);
+                        g.drawRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+                    }
+                }
+            }
+        }
+    }
+}
